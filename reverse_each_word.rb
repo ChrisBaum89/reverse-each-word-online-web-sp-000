56838c55f4bc4af2d120574952852b345aea93a2
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 02:12:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence = "Hello there, and how are you?"
 def reverse_each_word(sentence)
   new_array = sentence.split()
-  puts "#{new_array}"
+  #puts "#{new_array}"
   new_array.each do |i|
     word_array = i.chars
     #puts "#{word_array}"
