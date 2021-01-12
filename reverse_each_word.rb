83cbe08023d91219be89261397335fe8ed8276bd
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 02:23:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,14 @@
 #sentence = "Hello there, and how are you?"
 def reverse_each_word(sentence)
-  reverse_string_array = Array.new
-  reverse_sentence = ""
-  new_array = sentence.split()
-  #puts "#{new_array}"
+  reverse_string_array = Array.new #sets array
+  reverse_sentence = "" #sets variable
+  new_array = sentence.split() # splits sentence into an array of strings
   new_array.collect do |i|
-    word_array = i.chars
-    #puts "#{word_array}"
-    reverse_word_array = word_array.reverse
-    #puts "#{reverse_word_array}"
-    reverse_string_array << reverse_word_array.join
-    #puts "#{reverse_string}"
-    #reverse_sentence = reverse_string_array.join(" ")
-    #puts "#{reverse_sentence}"
+    word_array = i.chars  #creates an array of characters
+    reverse_word_array = word_array.reverse #reverses array of characters
+    reverse_string_array << reverse_word_array.join #joins array of characters into an array of strings
   end
-  reverse_sentence = reverse_string_array.join(" ")
+  reverse_sentence = reverse_string_array.join(" ") #joins the array of words into a string
 end
 
 #puts "#{reverse_each_word(sentence)}"
