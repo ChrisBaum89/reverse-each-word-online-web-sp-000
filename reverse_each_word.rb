5772f93089cacc125e8d25a5c3640ea82094bb2e
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 02:18:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 #sentence = "Hello there, and how are you?"
 def reverse_each_word(sentence)
   reverse_string_array = Array.new
-  var reverse_sentence
+  reverse_sentence = ""
   new_array = sentence.split()
   #puts "#{new_array}"
   new_array.collect do |i|
