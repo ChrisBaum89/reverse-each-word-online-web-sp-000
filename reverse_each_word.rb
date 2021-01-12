81d25f0e900ad2f1339bfc10f54630b090b0eabe
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 02:17:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 #sentence = "Hello there, and how are you?"
 def reverse_each_word(sentence)
-  reverse_string = Array.new
+  reverse_string_array = Array.new
+  var reverse_sentence
   new_array = sentence.split()
   #puts "#{new_array}"
   new_array.collect do |i|
@@ -8,9 +9,9 @@ def reverse_each_word(sentence)
     #puts "#{word_array}"
     reverse_word_array = word_array.reverse
     #puts "#{reverse_word_array}"
-    reverse_string << reverse_word_array.join
+    reverse_string_array << reverse_word_array.join
     #puts "#{reverse_string}"
-    reverse_sentence = reverse_string.join(" ")
+    reverse_sentence = reverse_string_array.join(" ")
     #puts "#{reverse_sentence}"
   end
   reverse_sentence
